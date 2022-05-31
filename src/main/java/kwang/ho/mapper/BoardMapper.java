fdf524fe498f170e533a415934cac4ee305bd5a4
skl5473 <skl5473@naver.com>
2022-05-31 23:03:27 +0900

    1. 답글 기능 2. 답글의 답글 기능 1) 정렬 순서 2) Level별 들여쓰기

--- src/main/java/kwang/ho/mapper/BoardMapper.java
+++ src/main/java/kwang/ho/mapper/BoardMapper.java
@@ -9,20 +9,40 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
 
+        // 게시판 목록
         List<BoardDto> selectBoardListWithPaging(PagingVO pagingVO) throws Exception;
 
-        int selectBoardTotalCount() throws Exception;
-
+        // 게시판 글쓰기
         void insertBoard(BoardDto board) throws Exception;
 
-        void updateHitCount(int bid) throws Exception;
-
+        // 게시판 상세보기
         BoardDto selectBoardDetail(int bid) throws Exception;
 
+        // 게시판 삭제
         void boardDelete(int bid) throws Exception;
 
+        // 게시판 수정페이지 호출
         BoardDto selectOpenBoardModify(int bid) throws Exception;
 
+        // 게시판 수정
         void boardModify(BoardDto board) throws Exception;
+
+        // 게시판 전체 갯수
+        int selectBoardTotalCount() throws Exception;
+
+        // 게시판 조회수
+        void updateHitCount(int bid) throws Exception;
+
+        // 게시판 답글 쓰기 페이지 호출
+        BoardDto selectBoardReplyWrite(BoardDto board) throws Exception;
+
+        // 게시판 답글 등록
+        int boardReply(BoardDto boardDto) throws Exception;
+
+        // 게시판 답글 순서 수정
+        void updateBoardReplyStep(BoardDto boardDto) throws Exception;
+
+        // 부모 reply_Level, reply_Step, bid_Parent 조회
+        BoardDto selectParentBoard(BoardDto boardDto) throws Exception;
 
 }
--- src/main/java/kwang/ho/service/BoardServiceImpl.java
+++ src/main/java/kwang/ho/service/BoardServiceImpl.java
@@ -14,41 +14,66 @@ public class BoardServiceImpl implements BoardService {
     @Autowired
     private BoardMapper boardMapper;
 
+    // 게시글 목록
     @Override
     public List<BoardDto> selectBoardListWithPaging(PagingVO pagingVO) throws Exception {
         return boardMapper.selectBoardListWithPaging(pagingVO);
     }
 
-    @Override
-    public int selectBoardTotalCount() throws Exception {
-        return boardMapper.selectBoardTotalCount();
-    }
-
-    @Override
-    public void insertBoard(BoardDto board) throws Exception {
-        boardMapper.insertBoard(board);
-    }
-
+    // 게시글 상세보기
     @Override
     public BoardDto selectBoardDetail(int bid) throws Exception {
         boardMapper.updateHitCount(bid);
         return boardMapper.selectBoardDetail(bid);
     }
 
+    // 게시글 등록
+    @Override
+    public void insertBoard(BoardDto board) throws Exception {
+
+        boardMapper.insertBoard(board);
+    }
+
+    // 게시글 삭제
     @Override
     public void boardDelete(int bid) throws Exception {
         boardMapper.boardDelete(bid);
     }
 
-    @Override
-    public void boardModify(BoardDto board) throws Exception {
-        boardMapper.boardModify(board);
-    }
-
+    // 게시글 수정페이지 호출
     @Override
     public BoardDto selectOpenBoardModify(int bid) throws Exception {
         return boardMapper.selectOpenBoardModify(bid);
     }
 
+    // 게시글 수정
+    @Override
+    public void boardModify(BoardDto board) throws Exception {
+        boardMapper.boardModify(board);
+    }
 
+    // 게시글 갯수
+    @Override
+    public int selectBoardTotalCount() throws Exception {
+        return boardMapper.selectBoardTotalCount();
+    }
+
+    // 게시판 답글 등록 페이지 호출
+    @Override
+    public BoardDto selectBoardReplyWrite(BoardDto board) throws Exception {
+        return boardMapper.selectBoardReplyWrite(board);
+    }
+
+    // 게시글 답글 등록
+    @Override
+    public int boardReply(BoardDto boardDto) throws Exception {
+
+        BoardDto parent = boardMapper.selectParentBoard(boardDto);
+        boardMapper.updateBoardReplyStep(parent);
+        boardDto.setBid_Parent(parent.getBid_Parent());
+        boardDto.setReply_Step(parent.getReply_Step()+1);
+        boardDto.setReply_Level(parent.getReply_Level()+1);
+
+        return boardMapper.boardReply(boardDto);
+    }
 }
