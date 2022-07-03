0edd878eb1a2617dfd7efe9b13840296fb7fd6dd
skl5473 <skl5473@naver.com>
2022-07-03 12:43:11 +0900

    Merge remote-tracking branch 'origin/master' into develop
    # Conflicts:
    #	src/main/resources/templates/작업.txt
    홈 화면 베스트셀러 롤링 완료

--- src/main/java/kwang/ho/controller/manage/ManageController.java
+++ src/main/java/kwang/ho/controller/manage/ManageController.java
@@ -6,14 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
